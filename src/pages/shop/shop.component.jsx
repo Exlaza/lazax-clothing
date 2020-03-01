b0d7efb4a90249ddd1b4ed054f4ebde1b202a43e
Ekansh Jain <ekansh.jain15@gmail.com>
2020-03-01 10:58:08 +0530

    feat: Promise based pattern instead of observable in Getting shop data

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -14,21 +14,18 @@ import WithSpinner from "../../components/with-spinner/with-spinner.component";
 const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
 const CollectionPageWithSpinner = WithSpinner(CollectionPage);
 
-
-
 class ShopPage extends React.Component {
-
   state = {
     loading: true
-  }
+  };
 
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
-    collectionRef.onSnapshot(async snapshot => {
+    collectionRef.get().then(snapshot => {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
-      this.setState({loading:false});
+      this.setState({ loading: false });
     });
   }
 
@@ -37,10 +34,18 @@ class ShopPage extends React.Component {
     const { loading } = this.state;
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} render={(props) => (<CollectionsOverviewWithSpinner isLoading={loading} {...props}  />)} />
+        <Route
+          exact
+          path={`${match.path}`}
+          render={props => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        />
         <Route
           path={`${match.path}/:collectionId`}
-          render={(props) => (<CollectionPageWithSpinner isLoading={loading} {...props} />)}
+          render={props => (
+            <CollectionPageWithSpinner isLoading={loading} {...props} />
+          )}
         />
       </div>
     );
